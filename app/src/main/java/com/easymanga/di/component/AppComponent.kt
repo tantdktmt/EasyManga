b0da0f852a313728dd949c20ba733bfffafb61da
td.tan <td.tan@alticast.com>
2020-06-19 14:58:33 +0700

    Add storage screen

--- app/src/main/java/com/easymanga/di/component/AppComponent.kt
+++ app/src/main/java/com/easymanga/di/component/AppComponent.kt
@@ -11,6 +11,7 @@ import com.easymanga.ui.base.BaseFragment
 import com.easymanga.ui.episodedetail.EpisodeDetailFragment
 import com.easymanga.ui.episodelist.page.EpisodeListFragment
 import com.easymanga.ui.mangalist.MangaListFragment
+import com.easymanga.ui.storage.StorageFragment
 import dagger.Component
 import javax.inject.Singleton
 
@@ -38,6 +39,8 @@ interface AppComponent {
     fun inject(fragment: EpisodeListFragment)
 
     fun inject(fragment: EpisodeDetailFragment)
+
+    fun inject(fragment: StorageFragment)
 
     fun inject(viewModel: SharedViewModel)
 }
--- app/src/main/java/com/easymanga/ui/MainActivity.kt
+++ app/src/main/java/com/easymanga/ui/MainActivity.kt
@@ -3,20 +3,21 @@ package com.easymanga.ui
 import android.content.res.Resources
 import android.os.Bundle
 import android.util.Log
-import androidx.appcompat.widget.Toolbar
+import android.view.View
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import com.easymanga.BuildConfig
 import com.easymanga.R
 import com.easymanga.ui.base.BaseActivity
 import com.easymanga.util.Constant
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : BaseActivity() {
 
     private lateinit var host: NavHostFragment
     private lateinit var appBarConfiguration: AppBarConfiguration
-    private lateinit var toolbar: Toolbar
     private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -27,14 +28,20 @@ class MainActivity : BaseActivity() {
                 as NavHostFragment? ?: return
         navController = host.navController
         appBarConfiguration = AppBarConfiguration(navController.graph)
-        toolbar = findViewById(R.id.toolbar)
         setSupportActionBar(toolbar)
+        bottom_nav_view.setupWithNavController(navController)
 
         navController.addOnDestinationChangedListener { _, destination, _ ->
+            if (destination.id == R.id.manga_list_dest || destination.id == R.id.storage_dest) {
+                bottom_nav_view.visibility = View.VISIBLE
+            } else {
+                bottom_nav_view.visibility = View.GONE
+            }
+
             val dest: String = try {
                 resources.getResourceName(destination.id)
             } catch (e: Resources.NotFoundException) {
-                Integer.toString(destination.id)
+                destination.id.toString()
             }
             if (BuildConfig.DEBUG) {
                 Log.d(Constant.LOG_TAG, "Navigated to $dest")
